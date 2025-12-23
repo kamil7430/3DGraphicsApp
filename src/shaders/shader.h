038721ad719d1253d0924f3e491a4eb0679295da
Kamil Błażejczyk <01189072@pw.edu.pl>
2025-12-23 22:36:38 +0100

    add Shader class

---
+++ src/shaders/shader.h
@@ -0,0 +1,17 @@
+#ifndef INC_3DGRAPHICSAPP_SHADER_H
+#define INC_3DGRAPHICSAPP_SHADER_H
+
+
+class Shader {
+private:
+    unsigned int shaderProgramId;
+
+public:
+    Shader(const char* vertexShaderFilePath, const char* fragmentShaderFilePath);
+    void use() const;
+    [[nodiscard]] int getUniformLocation(const char* uniformName) const;
+    ~Shader();
+};
+
+
+#endif //INC_3DGRAPHICSAPP_SHADER_H
