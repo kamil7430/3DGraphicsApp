388fa6b7229fb00b3a0d0b02e5f21cbbf7ca3490
Kamil Błażejczyk <01189072@pw.edu.pl>
2025-12-23 21:28:13 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
