68493dc8d13dfa22b206cc82fed7009b96a8d5cc
Kamil Błażejczyk <01189072@pw.edu.pl>
2025-12-24 12:00:47 +0100

    make further development

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,110 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <stb_image.h>
 
+#include "shaders/shader.h"
+
+int windowWidth = 800, windowHeight = 600;
+
+void framebuffer_size_callback(GLFWwindow *window, const int width, const int height)
+{
+    glViewport(0, 0, width, height);
+    windowWidth = width;
+    windowHeight = height;
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // Window creation
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow *window = glfwCreateWindow(windowWidth, windowHeight, "3D Scene", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        std::cerr << "Failed to create GLFW window!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (gladLoadGL(glfwGetProcAddress) == 0)
+    {
+        std::cerr << "Failed to initialize GLAD!" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glViewport(0, 0, windowWidth, windowHeight);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    stbi_set_flip_vertically_on_load(true);
+
+    // Data
+    constexpr float vertices1[] = {
+        // positions            // colors
+         1.0f,  1.0f,  1.0f,    1.0f, 0.0f, 0.0f, // v0 - red
+        -1.0f, -1.0f,  1.0f,    0.0f, 1.0f, 0.0f, // v1 - green
+        -1.0f,  1.0f, -1.0f,    0.0f, 0.0f, 1.0f, // v2 - blue
+         1.0f, -1.0f, -1.0f,    1.0f, 1.0f, 0.0f  // v3 - yellow
+    };
+
+    constexpr unsigned int indices1[] = {
+        0, 1, 2,
+        0, 3, 1,
+        0, 2, 3,
+        1, 3, 2
+    };
+
+    // OpenGL data objects
+    unsigned int VAO1;
+    glGenVertexArrays(1, &VAO1);
+    glBindVertexArray(VAO1);
+
+    unsigned int VBO1;
+    glGenBuffers(1, &VBO1);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO1);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
+
+    unsigned int EBO1;
+    glGenBuffers(1, &EBO1);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO1);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices1), indices1, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), reinterpret_cast<void *>(0));
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), reinterpret_cast<void *>(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    // Shaders
+    const Shader shader("./shaders/vertex/example.vert", "./shaders/fragment/example.frag");
+    shader.use();
+
+    glEnable(GL_DEPTH_TEST);
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+    // Main loop
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glBindVertexArray(VAO1);
+        glDrawArrays(GL_TRIANGLES, 0, sizeof(indices1) / 3);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
     return 0;
 }
