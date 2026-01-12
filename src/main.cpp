e5729b73ec3c0109e82526915806d52b22091f74
Kamil Blazejczyk <kamileczus@gmail.com>
2026-01-12 10:13:35 +0100

    Remove FragColor and use gl_FragColor instead

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,6 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     GLFWwindow *window = glfwCreateWindow(windowWidth, windowHeight, "3D Scene", nullptr, nullptr);
     if (window == nullptr)
--- src/shaders/fragment/example.frag
+++ src/shaders/fragment/example.frag
@@ -1,9 +1,8 @@
 #version 460 core
-out vec4 FragColor;
 
 in vec3 ourColor;
 
 void main()
 {
-    FragColor = vec4(ourColor, 1.0);
+    gl_FragColor = vec4(ourColor, 1.0);
 }
