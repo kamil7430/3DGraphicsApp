ea6adc07de2fbbfabadb3067002130b6eb215c91
Kamil Blazejczyk <kamileczus@gmail.com>
2026-01-12 10:30:12 +0100

    Change OpenGL version to 4.1

--- src/main.cpp
+++ src/main.cpp
@@ -31,8 +31,9 @@ int main() {
     // Window creation
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     GLFWwindow *window = glfwCreateWindow(windowWidth, windowHeight, "3D Scene", nullptr, nullptr);
     if (window == nullptr)
--- src/shaders/fragment/example.frag
+++ src/shaders/fragment/example.frag
@@ -1,8 +1,10 @@
-#version 460 core
+#version 410 core
 
 in vec3 ourColor;
 
+out vec4 FragColor;
+
 void main()
 {
-    gl_FragColor = vec4(ourColor, 1.0);
+    FragColor = vec4(ourColor, 1.0);
 }
