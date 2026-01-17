59a46cb14e5a3cf0b1ad910a812bca435658dae1
Kamil Błażejczyk <01189072@pw.edu.pl>
2026-01-17 09:09:37 +0100

    Add LightSource struct

--- src/light.h
+++ src/light.h
@@ -1,4 +1,11 @@
 #ifndef INC_3DGRAPHICSAPP_LIGHT_H
 #define INC_3DGRAPHICSAPP_LIGHT_H
 
+#include <glm/vec3.hpp>
+
+struct LightSource {
+    glm::vec3 position;
+    glm::vec3 color;
+};
+
 #endif //INC_3DGRAPHICSAPP_LIGHT_H
